087d05fa59afcf11bb524e5c73a100d9f154e2ae
Maverick-D-Aece <awesome.hamal@gmail.com>
2021-04-18 12:36:19 +0545

    Updated ThreadStydy.java

--- src/ThreadsStudy.java
+++ src/ThreadsStudy.java
@@ -7,6 +7,24 @@ public class ThreadsStudy {
         Thread runnableThread = new Thread(myRunnable);
         runnableThread.start();
 
+        Thread interruptedThread = new Thread(new InterruptedThread());
+        interruptedThread.start();
+        try {
+            Thread.sleep(5000);
+            interruptedThread.interrupt();
+        } catch (InterruptedException ex) {
+            System.err.println(ex.getMessage());
+        }
+
+        Thread threadJoin = new Thread(new ThreadJoin());
+        threadJoin.start();
+        try {
+            threadJoin.join();
+            // threadJoin.join(10000);
+        } catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        }
+
         System.out.println("\nMy main thread: " + Thread.currentThread().getName());
     }
 }
@@ -22,7 +40,6 @@ class MyThread extends Thread {
     public void run() {
         System.out.println("\nMy thread: " + getName());
     }
-
 }
 
 // creating a thread by implementing Runnable interface
@@ -30,7 +47,49 @@ class MyRunnable implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("\nMy runnable: " + Thread.currentThread().getName());
+        // pausing a thread
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            System.out.println("I have been interrrupted!");
+        }
+        System.out.println("\nSorry I'm late - My runnable: " + Thread.currentThread().getName());
     }
+}
 
+// creating an interruptable thread
+class InterruptedThread implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("This is mesage #" + i);
+
+            try {
+                Thread.sleep(2000);
+                continue;
+            } catch (InterruptedException e) {
+                System.out.println("I'm dead.");
+                return;
+            }
+        }
+    }
+}
+
+// creating a joinable thread
+class ThreadJoin implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("This is message #" + i);
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                System.out.println("I'm dead.");
+                return;
+            }
+        }
+    }
 }
